024ad274844725d668c3206b300f0cefd061e673
vigneshwaran-chandrasekaran <ac.vigneshwaran@gmail.com>
2020-02-08 17:36:42 +0530

    added folder for to save screenshots of the webpage

---
+++ github.js
@@ -0,0 +1,16 @@
+const puppeteer = require('puppeteer');
+const screenshot = `${Math.random()}_github.png`;
+(async () => {
+	const browser = await puppeteer.launch({ headless: false });
+	const page = await browser.newPage();
+	await page.goto('https://github.com/login', {
+		waitUntil: 'domcontentloaded',
+	});
+	await page.type('#login_field', 'testname');
+	await page.type('#password', 'testname');
+	await page.click('[name="commit"]');
+	await page.waitForNavigation();
+	await page.screenshot({ path: screenshot });
+	browser.close();
+	console.log('See screenshot: ' + screenshot);
+})();
---
+++ index.js
@@ -0,0 +1,30 @@
+const puppeteer = require('puppeteer');
+const iPhone = puppeteer.devices['iPhone 6'];
+
+const url = 'https://economictimes.indiatimes.com/';
+const screenshot = `screenshots/${Date.now()}_economictimes.png`;
+
+(async () => {
+	try {
+		const browser = await puppeteer.launch({ headless: false });
+		const page = await browser.newPage();
+		// await page.emulate(iPhone);
+		await page.goto(url, { waitUntil: 'domcontentloaded' });
+		// console.log(await page.content());
+		page.once('load', () => console.log('Page loaded!'));
+
+		const pageTitle = await page.title();
+
+		console.log(' page.title: ' + pageTitle);
+		await page.screenshot({
+			fullPage: true,
+			path: screenshot,
+		});
+
+		console.log('See screenshot: ' + screenshot);
+
+		await browser.close();
+	} catch (e) {
+		console.log('error', e);
+	}
+})();
